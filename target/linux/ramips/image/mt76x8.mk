61e9a4fca0bcaacd64a0d971a6418651e97f95ce
DSchndr <dennix1010@gmail.com>
2024-05-24 03:22:05 +0200

    [mt76x8] add gardena_smart_gateway_mt7688
    Signed-off-by: DSchndr <dennix1010@gmail.com>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -227,6 +227,24 @@ define Device/elecom_wrc-1167fs
 endef
 TARGET_DEVICES += elecom_wrc-1167fs
 
+define Device/gardena_smart_gateway_mt7688
+  IMAGE_SIZE := 114688k
+  KERNEL_SIZE := 16384k
+  DEVICE_VENDOR := GARDENA
+  DEVICE_MODEL := smart Gateway
+  DEVICE_PACKAGES:= uboot-envtools mtd-utils #kmod-ubi kmod-ubifs mtd-utils
+  DEVICE_DTS := mt7628an_gardena_smart_gateway_mt7688
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  #todo: find out why ubifs does not work :'(
+  #FILESYSTEMS += ubifs
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | check-size
+  KERNEL := kernel-bin | gzip | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+endef  
+TARGET_DEVICES += gardena_smart_gateway_mt7688 
+
 define Device/glinet_gl-mt300n-v2
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := GL.iNet
